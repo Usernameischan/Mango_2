d659fa57445f4e060b69d4cd6b0210c27e3e1cb7
Chan <chanhaen1999@gmail.com>
2023-04-01 21:44:04 +0900

    Update Server.py
    num_rounds = 10

--- server.py
+++ server.py
@@ -31,6 +31,6 @@ strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_av
 # Start Flower server
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=7),
+    config=fl.server.ServerConfig(num_rounds=10),
     strategy=strategy
 )
