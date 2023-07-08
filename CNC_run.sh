5cbf84cbe1a3f8ce9c681731261b1d047e6f18b9
SS-yong <sy5837@hufs.ac.kr>
2023-07-08 20:24:12 +0900

    [CHANGE]
    Do not work TPA-LSTM

--- CNC_run.sh
+++ CNC_run.sh
@@ -6,7 +6,7 @@ echo "Starting server"
 python CNC_server.py &
 sleep 3  # Sleep for 3s to give the server enough time to start
 
-for i in `seq 0 1`; do
+for i in `seq 0 1 2`; do
     echo "Starting client $i"
     python CNC_client.py $i &
 done
