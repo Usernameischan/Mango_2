944b14461c7aa0b136e7446a965d9b189ff8a040
SS-yong <sy5837@hufs.ac.kr>
2023-07-08 21:08:06 +0900

    [ADD] jax_regression

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -e
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
-
-# Download the CIFAR-10 dataset
-python -c "from torchvision.datasets import CIFAR10; CIFAR10('./data', download=True)"
 
 echo "Starting server"
 python server.py &
@@ -14,7 +9,7 @@ for i in `seq 0 1`; do
     python client.py &
 done
 
-# Enable CTRL+C to stop all background processes
+# This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 # Wait for all background processes to complete
 wait
